1b5a69453a0985191eb84c45cd728012ad2bd235
Sirma-cloud <kevinkiplimo99@gmail.com>
2024-02-14 00:41:22 +0300

    Created an endpoint to create a cart by adding a product to cart

--- src/main/java/com/example/shoppingcart/Controllers/ShoppingCartController.java
+++ src/main/java/com/example/shoppingcart/Controllers/ShoppingCartController.java
@@ -1,15 +1,27 @@
 package com.example.shoppingcart.Controllers;
 
+import com.example.shoppingcart.model.Cart;
 import com.example.shoppingcart.services.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/cart")
 public class ShoppingCartController {
     private final ShoppingCartService cartService;
 
     @Autowired
     public ShoppingCartController(ShoppingCartService cartService) {
         this.cartService = cartService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cart> createCart() {
+        Cart cart = cartService.createCart();
+        return new ResponseEntity<>(cart, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/shoppingcart/services/ShoppingCartService.java
+++ src/main/java/com/example/shoppingcart/services/ShoppingCartService.java
@@ -1,6 +1,7 @@
 package com.example.shoppingcart.services;
 
 import com.example.shoppingcart.Repositories.ShoppingCartRepository;
+import com.example.shoppingcart.model.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +12,10 @@ public class ShoppingCartService {
     @Autowired
     public ShoppingCartService(ShoppingCartRepository cartRepository) {
         this.cartRepository = cartRepository;
+    }
+
+    public Cart createCart() {
+        Cart cart = new Cart();
+        return cartRepository.save(cart);
     }
 }
